8f4860c9b551a3dbb898d7866ccc3af3aa604ee8
kyleoneill <kyleoneill24@gmail.com>
2024-06-28 11:51:49 -0400

    Update mvp for presentation

--- testing/mvp.lua
+++ testing/mvp.lua
@@ -1,7 +1,18 @@
+
 function test_function()
-    str_var = "Hello"
     int_var = 5
-    print(str_var .. int_var)
+    if int_var > 100 then
+        print("This is inside an if block")
+    elseif int_var > 1 then
+        print("This is inside an elseif")
+    else
+        print("This is inside an else")
+    end
+    hello_msg = "Hello, "
+    world_msg = "World!"
+    print(hello_msg .. world_msg)
+
+    print(hello_msg .. int_var)
 end
 
 test_function()
